4b42a5123514c161a5f0500b64fcd25d5f9bd3c8
NadirBakhsh <nadir4k2010@gmail.com>
2024-01-27 18:25:21 +0500

    tailwind css classes format added

--- prettier.config.js
+++ prettier.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  plugins: [
-    "prettier-plugin-tailwindcss",
-    "@trivago/prettier-plugin-sort-imports"
-  ],
-  tailwindConfig: './styles/tailwind.config.js',
-  trailingComma: "es5",
+  trailingComma: 'es5',
   tabWidth: 2,
   printWidth: 80,
   semi: false,
-  trailingComma: "all",
+  trailingComma: 'all',
   singleQuote: true,
-  importOrder: ["^@src/(.*)$"],
+  importOrder: ['^@src/(.*)$'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
+  tailwindConfig: './tailwind.config.ts',
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
 }
